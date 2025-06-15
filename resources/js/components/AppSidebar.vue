657b3982b016042f42f867a7e8617af13d3a92c1
Boukary DIALLO <agenceweb1919@gmail.com>
2025-06-15 15:37:15 +0000

    chore: run lint

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -2,46 +2,54 @@
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
+} from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, LayoutGrid, Package2, Pill, User, Users, ShoppingCart } from 'lucide-vue-next';
+import { BookOpen, LayoutGrid, Pill, ShoppingCart, User, Users } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
         href: '/dashboard',
-        icon: LayoutGrid,
+        icon: LayoutGrid
     },
     {
         title: 'Produits pharmaceutiques',
         href: '/pharmaceutical-products',
-        icon: Pill,
+        icon: Pill
     },
     {
         title: 'Pharmaciens',
         href: '/pharmacists',
-        icon: User,
+        icon: User
     },
     {
         title: 'Clients',
         href: '/clients',
-        icon: Users,
+        icon: Users
     },
     {
         title: 'Ventes',
         href: '/sales',
-        icon: ShoppingCart,
-    },
+        icon: ShoppingCart
+    }
 ];
 
 const footerNavItems: NavItem[] = [
     {
         title: 'Repository',
         href: 'https://github.com/BoubakarPi/dpharma.boukary.dev',
-        icon: BookOpen,
-    },
+        icon: BookOpen
+    }
 ];
 </script>
 
